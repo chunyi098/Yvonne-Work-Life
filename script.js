a97bb6fbc8ad082fe954f15d3fcd3bc6debd8ab2
chunyi <chunyi@dhc.com.sg>
2026-04-16 14:16:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+document.getElementById('receiptUpload').addEventListener('change', function(e) {
+    const file = e.target.files[0];
+    Tesseract.recognize(file, 'eng').then(({ data: { text } }) => {
+        // Simple logic to find a '$' or 'Total' in the text
+        const amountMatch = text.match(/\$?\d+(\.\d{2})?/);
+        if(amountMatch) document.getElementById('expAmount').value = amountMatch[0];
+        alert("Scanned text: " + text.substring(0, 100) + "...");
+    });
+});
